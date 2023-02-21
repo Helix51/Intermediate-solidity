c735af925ab631531126f0e633e5e01260ff7f03
Helix51 <105660340+Helix51@users.noreply.github.com>
2023-02-21 18:59:54 +0530

    Update event.sol

--- event.sol
+++ event.sol
@@ -2,10 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract Event {
-    // Event declaration
-    // Up to 3 parameters can be indexed.
-    // Indexed parameters helps you filter the logs by the indexed parameter
- 
+
     event messLog1(address indexed sender);
     event messLog2(uint indexed number,string message1);
     event messlog3(address indexed sender,string indexed message2);
