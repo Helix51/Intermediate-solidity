b5d196081f8d7e8eef9084895910bf41478ee654
Helix51 <105660340+Helix51@users.noreply.github.com>
2023-02-21 19:08:37 +0530

    Create globalfunctions.sol

---
+++ globalfunctions.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Event {
+
+ 
+    event messLog1(address sender,uint,uint );
+    function test()    payable  public {
+        emit messLog1(msg.sender,msg.value,1);
+        
+        
+        
+    }
+}
