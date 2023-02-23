4ebda552fb8cb20b7759912f50c9cade9d95ea92
Helix51 <105660340+Helix51@users.noreply.github.com>
2023-02-23 17:51:17 +0530

    Create connect_wallet_dapp.js

---
+++ connect_wallet_dapp.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import {ethers} from 'ethers'
+import './WalletCard.css'
+
+const WalletCard = () => {
+
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+			console.log('MetaMask Here!');
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+				getAccountBalance(result[0]);
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			
+			});
+
+		} else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+    
+}
+
+export default WalletCard;
